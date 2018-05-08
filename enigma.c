98f724222a9b8426a6cf6ae710e0b9395b3aebe6
Szymon Bednarek <behemkotel@gmail.com>
2018-05-08 09:05:02 +0200

    zarys projektu

---
+++ enigma.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char pierscien1(char l){
+    return l;
+}
+
+char pierscien2(char l){
+    return l;
+}
+
+void obroc(char p[]){
+
+}
+
+void szyruj(char t[], rozmiar){
+    int i;
+    for(i = 0; i < rozmiar; i++){
+        t[i] = pierscien2(pierscien1(t[i]));
+}
+
+void deszyfruj(char t[]){
+
+}
+
+
+int main(){
+
+
+    // czy szyrujemy czy deszyfrujemy
+
+    // wczytujemy tekst
+        // usun puste znaki (entery spacje)
+        // zmien litery na duże
+
+    // początkowe ustawienie pierscieni
+
+    // jezeli szyfrujemy
+    // szyfruj();
+
+    // jeżeli deszyfrujemy
+    // deszyfruj();
+
+    // wyświetlamy / zapisujemy do pliku
+}
